74c9039b606f83b332e388ab6c4fc26b5598531a
Eugene Marikhin <emarikhin@gmail.com>
2021-02-15 21:22:21 +1300

    extend formHandler

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import './styles.css'
 import {isValid} from "./utils";
+import {Questions} from "./questions";
 
 const form = document.getElementById('form') // getting the form from index.html
 const input = form.querySelector('#question-input') // passing the input event
@@ -11,6 +12,7 @@ input.addEventListener('input', () => { // constantly listening the input form a
 })
 
 function submitFormHandler(event) { // what happens when we hit the submit button
+    
     event.preventDefault() // prevent the page from default behaviour - restart the page and go to /? every time you submit the form
 
     if (isValid(input.value)) {
@@ -20,12 +22,11 @@ function submitFormHandler(event) { // what happens when we hit the submit butto
         }
 
         submitBtn.disabled = true; // disable the submit button as we've submitted the form hence drop it to default state.
-        // Async request to the server to save the question
-        console.log("Question: ", question) // print out the result to check what it looks like
 
-        input.value = '' // clear the input once the button's pressed
-        input.className = '' // remove the red binding once the question's been submitted;
-        // submitBtn.disabled = false
+        // Async request to the server to save the question
+        Questions.create(question).then(() => {
+            input.value = '' // clear the input once the button's pressed
+            input.className = '' // remove the red binding once the question's been submitted;
+        })
     }
-    // console.log(input.value)
 }
